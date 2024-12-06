ae3533d2a51ae701b61c41af05ec35894b68ec77
Tomasz Mroczko <tomekmr2001@gmail.com>
2024-12-06 12:36:51 +0100

    remove sensiteive information from spi

--- keycloakspi/src/main/java/com/spotspeak/zpi/Config.java
+++ keycloakspi/src/main/java/com/spotspeak/zpi/Config.java
@@ -1,13 +1,5 @@
 package com.spotspeak.zpi;
 
 public class Config {
-
-	public static final String DB_URL = "jdbc:postgresql://spotspeak-db.clmgo0u6o5n5.us-east-1.rds.amazonaws.com/spotspeak";
-	public static final String DB_USER = "spotspeak";
-	public static final String DB_PASS = "T0Od+VgsVdh6xnuDsT+Z4w==";
-	public static final String INSERT_USER_ENDPOINT = "http://34.118.97.103/api/users/init";
-	public static final String REALM_NAME = "spotspeak";
-	public static final String SERVER_URL = "http://localhost:8080";
-	public static final String CLIENT_ID = "spring-backend";
-	public static final String CLIENT_SECRET = "Dt5kASS6rs5r0DbSMWVDb3cTBixIfj1V";
+	public static final String INSERT_USER_ENDPOINT = System.getenv("INSERT_USER_ENDPOINT");
 }
